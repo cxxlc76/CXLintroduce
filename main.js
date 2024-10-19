7a90e8bb6ceeca755a7de08394e41c5261e56c17
cxxlc76 <2270398891@qq.com>
2024-10-20 00:04:29 +0800

    Update main.js

--- main.js
+++ main.js
@@ -119,7 +119,7 @@ function aboutMe(){
     if(i==0){
         i=1;
         slogan.style.height="100px";
-        slogan2.innerHTML="这里是高坂滑稽果，一个果厨~ 这个废柴能熟练掌握面向百度编程、Windows和Linux的开关机、PS和PR的安装和卸载、Arduino和各种零件的购买和损坏，还喜欢打音游（（（。。。生命不息，折腾不止！目前坐等高考中...";
+        slogan2.innerHTML="这里是陈熙霖，来自广西南宁，一个帅气的男孩，喜欢听音乐、看电影.";
     } else {
         i=0;
         slogan.style.height="50px";
